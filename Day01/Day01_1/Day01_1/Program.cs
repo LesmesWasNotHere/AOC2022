5e9c1bf52f7f30406c1f553666d0847166feba31
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-01 23:12:58 +0100

    First day part 1

---
+++ Day01/Day01_1/Day01_1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var lines = File.ReadAllLines("input");
+var maxElfCalories = 0;
+var currentElfCalories = 0;
+
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (currentElfCalories > maxElfCalories)
+            maxElfCalories = currentElfCalories;
+        currentElfCalories = 0;
+    }
+    else
+    {
+        currentElfCalories += int.Parse(line);
+    }
+}
+
+if(currentElfCalories > maxElfCalories)
+    maxElfCalories = currentElfCalories;
+
+Console.WriteLine($"Max elf calories: {maxElfCalories}");
