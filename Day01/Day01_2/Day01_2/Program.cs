c617d4e42acdab0751c8c512599adc73ae9c4b00
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-01 23:24:43 +0100

    Day 1 part 2

---
+++ Day01/Day01_2/Day01_2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+var lines = File.ReadAllLines("input");
+var elfCalories = new List<int>();
+var currentElfCalories = 0;
+
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elfCalories.Add(currentElfCalories);
+        currentElfCalories = 0;
+    }
+    else
+    {
+        currentElfCalories += int.Parse(line);
+    }
+}
+
+elfCalories.Add(currentElfCalories);
+
+var top3ElfCalories = elfCalories.OrderByDescending(x => x).Take(3).Sum();
+
+Console.WriteLine($"Top 3 elf calories: {top3ElfCalories}");
