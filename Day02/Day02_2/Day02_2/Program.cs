84f4847616d9dacaaacb21773b0701e1dcc5e5ae
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-02 23:31:45 +0100

    Day 2 part 2

---
+++ Day02/Day02_2/Day02_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// A Rock       1
+// B Paper      2
+// C Scissor    3
+
+// X Lose       0
+// Y Draw       3
+// Z Win        6
+
+var resultBias = new Dictionary<char, int>()
+{
+    { 'X', 2 },
+    { 'Y', 0 },
+    { 'Z', 1 }
+};
+
+int score = File.ReadAllLines("input.txt")
+    .Select(line => (line[0], line[2]))
+    .Select(item => { 
+            var result = ((item.Item1 - 'A') + resultBias[item.Item2]) % 3 + 1 + (item.Item2 - 'X') * 3; 
+            Console.WriteLine($"{item}  {result}");
+        return result;
+    })
+    .Sum();
+
+Console.WriteLine($"Score: {score}");
