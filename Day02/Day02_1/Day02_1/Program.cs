58d8cdb49cf722a36227a967212b5d1c3b261e33
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-02 22:48:07 +0100

    Day 2 part 1

---
+++ Day02/Day02_1/Day02_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// A X Rock
+// B Y Paper
+// C Z Scissors
+
+var winMoves = new HashSet<(char, char)>()
+{
+    ('A', 'B'),
+    ('B', 'C'),
+    ('C', 'A')
+};
+
+int score = File.ReadAllLines("input.txt")
+    .Select(line => (line[0], (char)(line[2] - 23)))
+    .Select(item =>
+    {
+        var score = item.Item2 - '@';
+
+        if (item.Item1 == item.Item2)
+            score += 3;
+        else
+            if (winMoves.Contains(item))
+                score += 6;
+        return score;
+
+    })
+    .Sum();
+
+Console.WriteLine($"Score: {score}");
