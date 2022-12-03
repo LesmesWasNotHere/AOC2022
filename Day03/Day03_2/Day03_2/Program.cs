9e1cc188957eec8a18d4a617f2612df4aeceda0e
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-03 23:04:20 +0100

    Day 3 part 2

---
+++ Day03/Day03_2/Day03_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+var badges = File.ReadLines("input.txt").ToArray().AsSpan<string>();
+var priority = 0;
+
+while (badges.Length > 0)
+{
+    var repeatedType = badges[..3].ToArray().Select(x => new HashSet<char>(x.ToCharArray())).Aggregate<HashSet<char>>((y, z) => y.Intersect(z).ToHashSet()).First();   
+    priority += char.IsLower(repeatedType) ? repeatedType - 96 : repeatedType - 'A' + 27;
+    badges = badges[3..];
+}
+
+Console.WriteLine($"Priority {priority}");
