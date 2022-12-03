3d0b007411c0f5ce01ef1aff39662828c6796a67
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-03 22:41:43 +0100

    Day 3 part 1

---
+++ Day03/Day03_1/Day03_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//8394
+using System.Linq;
+
+var priority = File.ReadLines("input.txt").Select(x =>
+{
+    var halfsize = x.Length / 2;
+    var compartment1 = new HashSet<char>(x.AsSpan()[..halfsize].ToArray());
+    var repeatedType = x.AsSpan()[halfsize..].ToArray().First(y => compartment1.Contains(y));
+    return char.IsLower(repeatedType) ? repeatedType - 96 : repeatedType - 'A' + 27;
+}).Sum();
+
+Console.WriteLine($"Priority {priority}");
