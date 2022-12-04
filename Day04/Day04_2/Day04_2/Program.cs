e859ca6786f53ccea2db656577c645d25addf0ab
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-04 13:01:18 +0100

    Day 4 part 2

---
+++ Day04/Day04_2/Day04_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+bool InRange(int number, int[] i)
+{
+    return i[0] <= number && i[1] >= number;
+}
+
+var overlaps = File.ReadAllLines("input.txt")
+    .Select(x => x.Split(",").Select(interval => interval.Split('-').Select(x => int.Parse(x)).ToArray()).ToArray())
+    .Where(x => InRange(x[0][0], x[1]) || InRange(x[0][1], x[1]) || InRange(x[1][0], x[0]) || InRange(x[1][1], x[0]))
+    .Count()
+    ;
+
+Console.WriteLine($"{overlaps} overlaps");
