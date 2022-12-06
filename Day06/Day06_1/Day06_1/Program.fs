5dff835fbeb1f58a5958ab3ddcbf4161bbd64d84
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-06 12:32:59 +0100

    Day 6 part 1

---
+++ Day06/Day06_1/Day06_1/Program.fs
@@ -0,0 +1,12 @@
+﻿let rec allDifferent (chars:string): bool =
+    match chars.Length with
+    | 0 | 1 -> true
+    | _ -> (allDifferent chars[1..]) && not (chars[1..].Contains chars[0])
+
+let rec findMarker (chars: string) initPos: int =
+    if (allDifferent chars[initPos..initPos+3]) then
+        initPos + 4
+    else
+        findMarker chars (initPos+1)
+
+printfn "Resultado: %d" ((System.IO.File.ReadAllText("input.txt"), 0) ||> findMarker)
