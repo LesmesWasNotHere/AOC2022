5fe3e6d3ae6e799c600e2734ba374ad8f1cf6fb4
lwnh <lesmes.gonzalez@coiipa.org>
2022-12-10 22:19:42 +0100

    Day 10 part 1

---
+++ Day10/Day10_1/Day10_1/Program.fs
@@ -0,0 +1,19 @@
+﻿let parseInstruction (i:string) =
+    if (i.Length > 4) then
+        [0; int(i[5..])]
+    else
+        [0]
+
+let rec getSignalStrength (input: int list) (currentIndex:int) (currentValue:int) (indexesToPick:int list): int list =
+    if input.IsEmpty || indexesToPick.IsEmpty then
+        []
+    else
+        let newValue = currentValue + input.Head
+        if (currentIndex = indexesToPick.Head) then
+            List.append [currentIndex*currentValue] (getSignalStrength input.Tail (currentIndex+1) newValue indexesToPick.Tail)
+        else
+            getSignalStrength input.Tail (currentIndex+1) newValue indexesToPick
+
+let input = System.IO.File.ReadAllLines("input.txt") |> Array.toList |> List.map parseInstruction |> List.concat
+
+printfn "Result: %d" (getSignalStrength input 1 1 [20..40..220] |> List.sum)
